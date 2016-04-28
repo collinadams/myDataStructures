654a7b36db61af578e60736f9383536c29ccf0bd
Collin Adams <collinadams04@gmail.com>
2016-04-28 16:19:03 -0700

    Finish Queue Function Instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,24 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    var numItemsInStorage = Object.keys(storage).length;
+    storage[numItemsInStorage] = value;
   };
 
   someInstance.dequeue = function(){
+    var numItemsInStorage = Object.keys(storage).length;
+    var dequeuedVal = storage['0'];
+    delete storage['0'];
+    for(var prop in storage){
+      storage[+prop - 1] = storage[prop]; 
+    }
+    delete storage[numItemsInStorage - 1];
+    return dequeuedVal;
   };
 
   someInstance.size = function(){
+    var numItemsInStorage = Object.keys(storage).length;
+    return numItemsInStorage;
   };
 
   return someInstance;
