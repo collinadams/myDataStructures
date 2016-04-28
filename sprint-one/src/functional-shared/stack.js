648e572828a9e90193f81ea2780c79f69dd305f3
Collin Adams <collinadams04@gmail.com>
2016-04-28 16:37:03 -0700

    Finish Stack Functional Shared Methods Instantiation

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.itemsInStorage = 0;
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
+
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+  this.storage[this.itemsInStorage] = value;
+  this.itemsInStorage++;
+};
+
+stackMethods.pop = function(){
+  this.itemsInStorage--;
+  var poppedVal = this.storage[this.itemsInStorage];
+  delete this.storage[this.itemsInStorage];
+  return poppedVal;
+};
+
+stackMethods.size = function(){
+  return Math.max(this.itemsInStorage, 0);
+};
 
 
