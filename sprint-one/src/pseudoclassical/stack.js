90e3d25cc391454803d5c84e5620113e2ea2ab6d
Collin Adams <collinadams04@gmail.com>
2016-04-29 14:53:02 -0700

    Finish Stack Pseudoclassical Instantiation

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.itemsInStorage = 0;
 };
 
+Stack.prototype.push = function(value){
+  this.storage[this.itemsInStorage] = value;
+  this.itemsInStorage++;
+};
 
+Stack.prototype.pop = function(){
+  if(this.itemsInStorage > 0){
+    this.itemsInStorage--;
+  }
+  var poppedVal = this.storage[this.itemsInStorage];
+  delete this.storage[this.itemsInStorage];
+  return poppedVal;
+};
+
+Stack.prototype.size = function(){
+  return this.itemsInStorage;
+}
