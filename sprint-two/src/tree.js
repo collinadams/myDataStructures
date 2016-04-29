48493e92aeec39d50e5ef534c0548bc60f776c95
Collin Adams <collinadams04@gmail.com>
2016-04-29 15:48:05 -0700

    Finish Tree, addChild, and contains in Functional Instantiation Pattern

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,23 +3,43 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  extend(newTree, treeMethods);
   return newTree;
 };
 
-
+var extend = function(recipientObj, donorObj){
+  for(var prop in donorObj){
+    recipientObj[prop] = donorObj[prop];
+  }
+}
 
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+  if(this.value === target){
+    return true;
+  }
 
+  var breadthFirstQueue = [];
+  breadthFirstQueue.push({node: this, depth: 0});
+
+  while(currentNode = breadthFirstQueue.shift()){
+    if(currentNode.node.value === target){
+      return true;
+    }
+    for(var i = 0; i < currentNode.node.children.length; i++){
+      breadthFirstQueue.push({node: currentNode.node.children[i], depth: currentNode.depth + 1});
+    }
+  }
+
+  return false;
 };
 
 
