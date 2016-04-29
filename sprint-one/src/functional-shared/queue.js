918dfcb492c2f9eb97c2baefbbb1ea824f84c83c
Collin Adams <collinadams04@gmail.com>
2016-04-28 17:06:32 -0700

    Instantiate Queue in Constant Time with Shared Methods

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,33 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  _.extend(someInstance, queueMethods);
+  someInstance.storage = {};
+  someInstance.start = -1;
+  someInstance.end = -1;
+
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this.end++;
+  this.storage[this.end] = value;
+};
+
+queueMethods.dequeue = function(){
+  this.size() && this.start++;
+  var dequeuedVal = this.storage[this.start];
+  delete this.storage[this.start];
+  return dequeuedVal; 
+};
+
+queueMethods.size = function(){
+  return this.end - this.start;
+}
 
 
 
