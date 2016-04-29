431266de4f4b82d9af817af5d552b8c525098c58
Collin Adams <collinadams04@gmail.com>
2016-04-29 14:32:41 -0700

    Finish Queue Prototypal Instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newInstance = Object.create(queueMethods);
+  newInstance.storage = {};
+  newInstance.start = -1;
+  newInstance.end = -1;
+
+  return newInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this.end++;
+  this.storage[this.end] = value;
+};
+
+queueMethods.dequeue = function(){
+  if(this.size() > 0){
+    this.start++;
+  }
+  var dequeuedVal = this.storage[this.start];
+  delete this.storage[this.start];
+  return dequeuedVal;
+};
+
+queueMethods.size = function(){
+  return this.end - this.start;
+}
 
 
