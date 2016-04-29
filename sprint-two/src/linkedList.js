89cc9e183a67c6bbfc9b1c6b528b98e67ede8ba5
Collin Adams <collinadams04@gmail.com>
2016-04-29 15:24:17 -0700

    Finish Linked List in Functional Instantiation Pattern

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if(list.tail){
+      list.tail.next = newNode;
+    }else{
+      list.head = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    if(!list.head){
+      return null;
+    }
+
+    var oldHead = list.head;
+    list.head = list.head.next;
+    oldHead.next = null;
+    return oldHead.value;
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+    while(currentNode){
+      if(currentNode.value === target){
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
