82fc011f39b18c179cff6f2f22bb1c9a4960ddcd
Collin Adams <collinadams04@gmail.com>
2016-04-29 14:18:55 -0700

    Finish Stack Prototypal Instantiation

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+
+  obj.storage = {};
+  obj.itemsInStorage = 0;
+
+  return obj;
+
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+  this.storage[this.itemsInStorage] = value;
+  this.itemsInStorage++;
+};
 
+stackMethods.pop = function(){
+  if(this.itemsInStorage > 0){
+    this.itemsInStorage--;
+  }
+  var poppedVal = this.storage[this.itemsInStorage];
+  delete this.storage[this.itemsInStorage];
+  return poppedVal;
+}
+
+stackMethods.size = function(){
+  return this.itemsInStorage;
+}
