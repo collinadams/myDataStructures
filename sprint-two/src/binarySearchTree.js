676dc2fb66a27e40c342bbd51dd6da4b3fd27d87
Collin Adams <collinadams04@gmail.com>
2016-04-29 17:15:39 -0700

    Finish Binary Tree Search, Insert, and depthFirstLog in Prototypal Instantiation Pattern

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,64 @@
 var BinarySearchTree = function(value){
+  var binaryTree = Object.create(BinarySearchTreePrototype);
+
+  binaryTree.value = value;
+  binaryTree.left = null;
+  binaryTree.right = null;
+
+  return binaryTree;
 };
+
+var BinarySearchTreePrototype = {};
+
+BinarySearchTreePrototype.insert = function(value){
+  if(this.value > value){
+    if(!this.left){
+      this.left = BinarySearchTree(value);
+    }else{
+      this.left.insert(value);
+    }
+  }else if(this.value < value){
+    if(!this.right){
+      this.right = BinarySearchTree(value);
+    }else{
+      this.right.insert(value);
+    }
+  }else{
+    return 'The tree already contains this value';
+  }
+};
+
+BinarySearchTreePrototype.contains = function(target){
+  if(this.value === target){
+    return true;
+  }else if(target < this.value){
+    if(!this.left){
+      return false;
+    }else{
+      return this.left.contains(target);
+    }
+  }else if(target > this.value){
+    if(!this.right){
+      return false;
+    }else{
+      return this.right.contains(target)
+    }
+  }else{
+    return false;
+  }
+};
+
+BinarySearchTreePrototype.depthFirstLog = function(callback){
+  callback(this.value);
+
+  if(this.left){
+    this.left.depthFirstLog(callback);
+  }
+  if(this.right){
+    this.right.depthFirstLog(callback);
+  }
+};
+
 
 
 /*
